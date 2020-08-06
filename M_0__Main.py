d2a7a26027c26f7dca2cb1307066ab37fccfcc9b
Thorsten <47179219+DevThorDev@users.noreply.github.com>
2020-08-06 23:09:50 +0200

    Add files via upload

--- M_0__Main.py
+++ M_0__Main.py
@@ -22,12 +22,14 @@ inDG.addObjTps(S_OBJINP)
 print('Added object types.')
 
 cDatFilt = DataFilter(inDG)
-print('Starting filtering', cDatFilt.getValDITp('sTask2'), '...')
-dfrSortFilt = cDatFilt.filterDataFrame()
+print('Starting filtering...')
+dDfrSortFilt = cDatFilt.filterDataFrames()
+print('Done filtering', cDatFilt.getValDITp('sTask2'), '.')
 if inDG.dI['doNetworkPlot']:
+    print('Starting network plotting...')
     cNetwPltr = NetworkPlotter(inDG)
-    print('Starting network plot', cNetwPltr.getValDITp('sFNetwPlt'), '...')
-    cNetwPltr.createSaveFigure(dfrSortFilt)
+    cNetwPltr.createSaveFigures(dDfrSortFilt)
+    print('Done network plot', cNetwPltr.getValDITp('sFNetwPlt'), '...')
 
 # -----------------------------------------------------------------------------
 GF.endSimu(startTime)
